610cde8debe477307308a1e3015711b59188a31c
odiseah12 <113215622+odiseah12@users.noreply.github.com>
2023-03-03 16:45:27 -0300

    Add files via upload

---
+++ web online/js/js.js
@@ -0,0 +1,8 @@
+
+
+
+let menu = document.getElementById("menu");
+let menuBar = document.getElementById("menu-bar");
+menuBar.addEventListener("click", function(){
+    menu.classList.toggle("menu-toggle")
+});
