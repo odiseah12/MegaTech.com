4cfffdbde4e28e17885e95e916167fea2144b073
odiseah12 <113215622+odiseah12@users.noreply.github.com>
2023-03-03 16:49:20 -0300

    Delete web online directory

--- web online/js/js.js
+++
@@ -1,8 +0,0 @@
-
-
-
-let menu = document.getElementById("menu");
-let menuBar = document.getElementById("menu-bar");
-menuBar.addEventListener("click", function(){
-    menu.classList.toggle("menu-toggle")
-});
